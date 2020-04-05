c5c4316868cd8fe13de5737cbfc870806425bc3a
keeprich <keeprich@keeprichs-air.lan>
2020-04-05 18:10:24 +0100

    Updates

---
+++ precache-manifest.2105bf2719b9e7ed9170b26120daa546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47941ba0757c7dea4b0a3ee26927f570",
+    "url": "/food-recipe-app/index.html"
+  },
+  {
+    "revision": "a026e0f0c86748ead087",
+    "url": "/food-recipe-app/static/css/main.6b8ba005.chunk.css"
+  },
+  {
+    "revision": "1b8683a93f3e655fb18a",
+    "url": "/food-recipe-app/static/js/2.f9335df7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe-app/static/js/2.f9335df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a026e0f0c86748ead087",
+    "url": "/food-recipe-app/static/js/main.9270af0d.chunk.js"
+  },
+  {
+    "revision": "3c439716f471e65542b3",
+    "url": "/food-recipe-app/static/js/runtime-main.2603eafb.js"
+  }
+]);
